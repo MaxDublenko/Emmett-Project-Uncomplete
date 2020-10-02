11b2f69d6a33ab40868226ac732348f209021ebf
MaxDublenko <55203253+MaxDublenko@users.noreply.github.com>
2020-10-02 12:51:46 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -20,10 +20,12 @@ function drawChart(h1, f1, e1, h2, w1, e2) {
   ctx.arc(cnv.width/2, cnv.height/2, cnv.width/2, 0, 2 * Math.PI);
   ctx.fill();
   let start = 0;
+  // Move sections to center of circle
   for (let i = 0; i < 6; i++) {
     ctx.fillStyle = colors[i];
     start += Math.PI / 6;
-    ctx.beginPath(cnv.width/2, cnv.height/2, cnv.width/2, start, 2 * Math.PI);
+    ctx.beginPath();
+    ctx.arc(cnv.width/2, cnv.height/2, cnv.width/2, start, 2 * Math.PI);
     ctx.fill();
   }
 }
